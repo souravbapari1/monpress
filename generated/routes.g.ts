59fe0d15ad1f538b88d1775bfe27852f433ce9e1
Sourav <codexsourav404@gmail.com>
2025-03-04 00:54:50 +0530

    update

--- generated/routes.g.ts
+++ generated/routes.g.ts
@@ -2,7 +2,11 @@ import { Router } from "express";
 const router = Router();
 
 //Route Group /  ✅
-import * as routes0 from "@/src/routes/index.ts";
-router["get" as "get" | "post" | "put" | "patch" | "delete"]("/", routes0.GET);
+import * as routes0 from '@/src/routes/index.ts'; 
+router.get("/", routes0.GET);
+router.post("/", routes0.POST);
+router.patch("/", routes0.PATCH);
+router.put("/", routes0.PUT);
+router.delete("/", routes0.DELETE);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,21 @@
 import { httpRequest } from "monpress";
 
 export const GET = httpRequest(async (req, res) => {
-  res.send("Hello World");
+  res.json({ message: "GET request successful" });
+});
+
+export const POST = httpRequest(async (req, res) => {
+  res.json({ message: "POST request successful" });
+});
+
+export const PATCH = httpRequest(async (req, res) => {
+  res.json({ message: "PATCH request successful" });
+});
+
+export const PUT = httpRequest(async (req, res) => {
+  res.json({ message: "PUT request successful" });
+});
+
+export const DELETE = httpRequest(async (req, res) => {
+  res.json({ message: "DELETE request successful" });
 });

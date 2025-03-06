5928282df78a260a0b48ee0a1e8028e51b64b444
Sourav <codexsourav404@gmail.com>
2025-03-06 14:02:49 +0530

    add http

--- main.ts
+++ main.ts
@@ -5,9 +5,10 @@ import { authMiddleware } from "@/src/middleware/authMiddleware";
 const mon = MonPress({
   routes,
   middleware: [authMiddleware],
+  express(app, http) {},
 });
 
 const port = process.env.PORT || 3000;
-mon.listen(port, (e) => {
-  console.log("⚡️ Server is running on port http://localhost:" + port);
+mon.listen(port, () => {
+  console.log(`⚡️ Server is running on port http://localhost:${port}`);
 });
